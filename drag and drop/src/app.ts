bdbb6af23bd2caadb3ecd1e567d312dd51b7d996
LaupWing <loc_strict@hotmail.com>
2021-03-17 14:51:43 +0100

    123

--- drag and drop/src/app.ts
+++ drag and drop/src/app.ts
@@ -2,6 +2,9 @@ class ProjectInput {
    templateElement: HTMLTemplateElement
    hostElement: HTMLDivElement
    element: HTMLFormElement
+   titleInputElement: HTMLInputElement
+   descriptionInputElement: HTMLInputElement
+   peopleInputElement: HTMLInputElement
 
    constructor() {
       this.templateElement = <HTMLTemplateElement>document.getElementById('project-input')
@@ -9,11 +12,25 @@ class ProjectInput {
 
       const importedNode = document.importNode(this.templateElement.content, true)
       this.element = importedNode.firstElementChild as HTMLFormElement
+      this.element.id = 'user-input'
+      this.titleInputElement = <HTMLInputElement> this.element.querySelector('#title')
+      this.descriptionInputElement = <HTMLInputElement> this.element.querySelector('#description')
+      this.peopleInputElement = <HTMLInputElement> this.element.querySelector('#people')
       this.attach()
+      this.configure()
+   }
+
+   private submitHandler(event: Event){
+      event.preventDefault()
+      console.log(this.titleInputElement.value)
    }
 
    private attach(){
       this.hostElement.insertAdjacentElement('afterbegin', this.element)
+   }
+
+   private configure(){
+      this.element.addEventListener('submit', this.submitHandler.bind(this))
    }
 }
 
