8bd405f773fdc1f4e8b3a196a6e1ebb420ba803d
LaupWing <loc_strict@hotmail.com>
2021-03-22 19:56:31 +0100

    133

--- drag and drop/src/app.ts
+++ drag and drop/src/app.ts
@@ -129,6 +129,11 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
 
 class ProjectItem extends Component<HTMLUListElement, HTMLLIElement>{
    private project: Project
+
+   get persons(){
+      return this.project.people === 1 ? '1 person' : `${this.project.people} persons`
+   }
+
    constructor(
       hostId: string,
       project: Project
@@ -142,7 +147,7 @@ class ProjectItem extends Component<HTMLUListElement, HTMLLIElement>{
    configure(){}
    renderContent(){
       this.element.querySelector('h2')!.textContent = this.project.title
-      this.element.querySelector('h3')!.textContent = this.project.people.toString()
+      this.element.querySelector('h3')!.textContent = this.persons +'assigned.'
       this.element.querySelector('p')!.textContent = this.project.description
    }
 }
