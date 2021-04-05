9ef848791bae761690afaec3fbeccd429e35249b
LaupWing <loc_strict@hotmail.com>
2021-04-05 15:39:27 +0200

    183

--- react-typescript/components/TodoList.tsx
+++ react-typescript/components/TodoList.tsx
@@ -5,10 +5,19 @@ interface TodoListProps {
       id: string,
       text: string
    }[]
+   onDeleteTodo:(todoId:string)=>void
 }
 
-const TodoList: React.FC<TodoListProps> = ({items}) =>{
-   return <ul>{items.map(todo => <li key={todo.id}>{todo.text}</li> )}</ul>
+const TodoList: React.FC<TodoListProps> = ({items, onDeleteTodo}) =>{
+   return (
+      <ul>{items.map(todo => 
+         <li key={todo.id}>
+            <span>{todo.text}</span>
+            <button onClick={()=>onDeleteTodo(todo.id)}>Delete</button>
+         </li> 
+         )}
+      </ul>
+   )
 }
 
 export default TodoList
