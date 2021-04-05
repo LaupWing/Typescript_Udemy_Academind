e8a4ec7845f63758c98755408d74fa58c2807f2f
LaupWing <loc_strict@hotmail.com>
2021-04-05 20:09:02 +0200

    193

--- node-typescript/src/app.ts
+++ node-typescript/src/app.ts
@@ -1,7 +1,8 @@
 import express, {Request, Response, NextFunction} from 'express'
 const app = express()
 import todoRoutes from './routes/todos'
-
+import {json} from 'body-parser'
+app.use(json())
 app.use('/todos', todoRoutes)
 app.use((err: Error, req: Request, res: Response, next: NextFunction)=>{
    res.status(500).json({message: err.message})
